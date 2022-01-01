bb9ebb508d2daaa72068918a6a951abf97430521
Augusto Ribeiro <augustoribeiro32@gmail.com>
2022-01-01 20:26:28 -0300

    finalizei o preenchimento dos numeros pressionados e o processo de mostrar na tela

---
+++ script/script.js
@@ -0,0 +1,39 @@
+const tela = document.querySelector('#tela');
+const historicoCalculoAtual = document.querySelector('#historico-calculo-atual');
+const numeros = document.querySelectorAll('.numeros');
+const operacoes = document.querySelectorAll('.operacoes');
+const numerosPressionados = [];
+
+(function preencherNumerosPressionados() {
+  numeros.forEach(numero => {
+    numero.addEventListener('click', ()=>{
+      numerosPressionados.push(numero.textContent);
+      if (numerosPressionados.length > 0) {
+        let zero = document.querySelector('#zero');
+        zero.disabled = false;
+        zero.enabled = true;
+      }
+    });
+  });
+  return numerosPressionados;
+})();
+
+(function mostrarNumerosPressionados() {
+  numeros.forEach(numero => {
+    numero.addEventListener('click', ()=>{ 
+      if (numerosPressionados.length == 1) {
+        tela.textContent = tela.textContent.replace(tela.textContent, numerosPressionados[0]);
+      }
+      else {
+        tela.textContent += numerosPressionados[numerosPressionados.length - 1];
+      }
+
+      if (tela.textContent.includes(',')) {
+        let virgula = document.querySelector('#virgula');
+        virgula.enabled = false;
+        virgula.disabled = true;
+      }
+    });
+  }); 
+  return tela.textContent;
+})();
